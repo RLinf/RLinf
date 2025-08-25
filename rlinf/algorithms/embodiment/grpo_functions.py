fix: fix embodiment grad accum calculation; fix embodiment training logging; fix some cfgs

rlinf/algorithms/embodiment/grpo_functions.py
@@ -265,19 +265,14 @@ def actor_loss_fn(
             - actor/pg_clipfrac: Fraction of clipped policy gradient loss
             - actor/ppo_kl: Approximate KL divergence
     """
-    # Compute approximate KL divergence
-    negative_approx_kl = log_probs - old_log_prob  # [bs, ] | [bs, len]
-
-    # Compute probability ratio for PPO clipping
-    ratio = torch.exp(negative_approx_kl)  # [bs, ] | [bs, len]
-    if len(ratio.shape) == 1:
-        ratio = ratio.unsqueeze(-1)
+    logratio = log_probs - old_log_prob
+    ratio = torch.exp(logratio)
 
     # Compute clipped and unclipped policy gradient losses
-    pg_losses = -advantages * ratio  # [bs, len]
+    pg_losses = -advantages * ratio
     pg_losses2 = -advantages * torch.clamp(
         ratio, 1.0 - clip_ratio_low, 1.0 + clip_ratio_high
-    )  # [bs, len]
+    )
 
     if loss_mask is not None:
         # Take the maximum of clipped and unclipped losses
@@ -294,8 +289,8 @@ def actor_loss_fn(
 
     # Compile metrics for logging
     metrics_data = {
-        "actor/loss": pg_loss.detach().item(),
-        "actor/pg_loss": pg_loss.detach().item(),
-        "actor/pg_clipfrac": pg_clipfrac.detach().item(),
+        "actor/raw_loss": pg_loss.detach().item(),
+        "actor/policy_loss": pg_loss.detach().item(),
+        "actor/policy_clipfrac": pg_clipfrac.detach().item(),
     }
     return pg_loss, metrics_data

rlinf/workers/actor/fsdp_actor_worker.py
@@ -37,6 +37,7 @@ from rlinf.models.embodiment.model_utils import custom_forward
 from rlinf.scheduler import Worker
 from rlinf.utils.data_iter_utils import get_iterator_k_split
 from rlinf.utils.placement import HybridComponentPlacement
+from rlinf.utils.distributed import all_reduce_dict
 
 
 class EmbodiedFSDPActor(FSDPModelManager, Worker):
@@ -219,16 +220,19 @@ class EmbodiedFSDPActor(FSDPModelManager, Worker):
                 value = value.reshape(rollout_size, *value.shape[2:])
                 self.rollout_batch[key] = value[shuffle_id]
 
-        assert self.cfg.actor.global_batch_size % self.cfg.actor.micro_batch_size == 0
+        assert self.cfg.actor.global_batch_size % (
+            self.cfg.actor.micro_batch_size * self._world_size
+        ) == 0
+
         self.gradient_accumulation = (
-            self.cfg.actor.global_batch_size // self.cfg.actor.micro_batch_size
+            self.cfg.actor.global_batch_size // self.cfg.actor.micro_batch_size // self._world_size
         )
 
         # Split to make minibatch iterator for updating the actor
         # See PPO paper for details. https://arxiv.org/abs/1707.06347
         rollout_size = self.rollout_batch["input_ids"].size(0)
         batch_size_per_rank = (
-            self.cfg.actor.global_batch_size // torch.distributed.get_world_size()
+            self.cfg.actor.global_batch_size // self._world_size
         )
         assert rollout_size % batch_size_per_rank == 0, (
             f"{rollout_size} is not divisible by {batch_size_per_rank}"
@@ -323,6 +327,7 @@ class EmbodiedFSDPActor(FSDPModelManager, Worker):
                 loss /= self.gradient_accumulation
                 loss.backward()
 
+                metrics_data["loss"] = loss.detach().item()
                 append_to_dict(metrics, metrics_data)
 
             torch.cuda.empty_cache()
@@ -342,6 +347,8 @@ class EmbodiedFSDPActor(FSDPModelManager, Worker):
             append_to_dict(metrics, data)
 
         mean_metric_dict = {key: np.mean(value) for key, value in metrics.items()}
+        mean_metric_dict = all_reduce_dict(mean_metric_dict, op=torch.distributed.ReduceOp.AVG)
+        
         self.optimizer.zero_grad()
         torch.cuda.synchronize()
         torch.distributed.barrier()