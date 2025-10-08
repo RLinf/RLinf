fix(embodied): change the default gradient ckpt as True; add yaml for libero90 and libero130; change the way of overriding get_benchmark;
Signed-off-by: weimingjie <weimingjie@infini-ai.com>

rlinf/envs/libero/libero_env.py
@@ -20,12 +20,12 @@ import gym
 import numpy as np
 import torch
 from libero.libero import get_libero_path
-from libero.libero.benchmark import Benchmark, get_benchmark
+from libero.libero.benchmark import Benchmark
 from libero.libero.envs import OffScreenRenderEnv
 from omegaconf.omegaconf import OmegaConf
 
 from rlinf.envs.libero.utils import (
-    _install_get_benchmark_override,
+    _get_benchmark_overridden,
     get_libero_image,
     get_libero_wrist_image,
     list_of_dict_to_dict_of_list,
@@ -57,8 +57,7 @@ class LiberoEnv(gym.Env):
         self._generator_ordered = np.random.default_rng(seed=0)
         self.start_idx = 0
 
-        self._init_libero()
-        self.task_suite: Benchmark = get_benchmark(cfg.task_suite_name)()
+        self.task_suite: Benchmark = _get_benchmark_overridden(cfg.task_suite_name)()
 
         self._compute_total_num_group_envs()
         self.reset_state_ids_all = self.get_reset_state_ids_all()
@@ -75,9 +74,6 @@ class LiberoEnv(gym.Env):
         self.video_cfg = cfg.video_cfg
         self.video_cnt = 0
         self.render_images = []
-
-    def _init_libero(self):
-        _install_get_benchmark_override()
 
     def _init_env(self):
         env_fns = self.get_env_fns()