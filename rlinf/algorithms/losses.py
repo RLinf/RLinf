cf49c3e34f4dcbb5eb2c462efe1f96b67f872bfe
Florielle <1205402283@qq.com>
2025-10-11 15:04:48 +0800

    feat: fix embodiment grpo
    Signed-off-by: Florielle <1205402283@qq.com>

--- rlinf/algorithms/losses.py
+++ rlinf/algorithms/losses.py
@@ -52,10 +52,12 @@ def compute_ppo_actor_loss(
         Tuple[torch.Tensor, Dict]: (actor_loss, metrics_dict)
     """
 
-    if max_episode_steps is not None and loss_mask_sum is not None and loss_mask is not None:
-        loss_mask_ratio = (
-            (loss_mask_sum * 1.0) / max_episode_steps
-        )
+    if (
+        max_episode_steps is not None
+        and loss_mask_sum is not None
+        and loss_mask is not None
+    ):
+        loss_mask_ratio = (loss_mask_sum * 1.0) / max_episode_steps
         loss_agg_func = masked_mean_ratio
 
     if loss_mask is None:
