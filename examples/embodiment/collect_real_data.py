907cfb9b15be1bc73e336e37881e8b5ceef6ca74
hongzhi <zanghz21@mails.tsinghua.edu.cn>
2025-12-10 01:40:27 +0800

    fix: add script

--- examples/embodiment/collect_real_data.py
+++ examples/embodiment/collect_real_data.py
@@ -10,6 +10,7 @@ import numpy as np
 import hydra
 from rlinf.scheduler import Cluster
 from rlinf.utils.placement import HybridComponentPlacement
+import copy
 
 @hydra.main(
     version_base="1.1", config_path="config", config_name="real_collect_data"
@@ -18,7 +19,7 @@ def main(cfg):
     cluster = Cluster(cluster_cfg=cfg.cluster)
     component_placement = HybridComponentPlacement(cfg, cluster)
 
-    success_needed = 1
+    success_needed = 10
     success_cnt = 0
     env = gym.make(
         "PegInsertionEnv-v1", 
@@ -34,12 +35,46 @@ def main(cfg):
     env = RelativeFrame(env)
     env = Quat2EulerWrapper(env)
 
+    transitions = []
+
     env.reset()
     print("Start collecting data...")
     while success_cnt < success_needed:
         action = np.zeros((6,))
-        env.step(action)
-    
+        next_obs, rew, done, truncated, info = env.step(action)
+        if "intervene_action" in info:
+            actions = info["intervene_action"]
+
+        transition = copy.deepcopy(
+            dict(
+                observations=obs,
+                actions=actions,
+                next_observations=next_obs,
+                rewards=rew,
+                masks=1.0 - done,
+                dones=done,
+            )
+        )
+        transitions.append(transition)
+
+        obs = next_obs
+
+        if done:
+            success_cnt += rew
+            total_count += 1
+            print(
+                f"{rew}\tGot {success_cnt} successes of {total_count} trials. {success_needed} successes needed."
+            )
+            # pbar.update(rew)
+            obs, _ = env.reset()
+
+    # with open(file_path, "wb") as f:
+    #     pkl.dump(transitions, f)
+    #     print(f"saved {success_needed} demos to {file_path}")
+
+    env.close()
+    # pbar.close()
+
 
 if __name__ == "__main__":
     main()
