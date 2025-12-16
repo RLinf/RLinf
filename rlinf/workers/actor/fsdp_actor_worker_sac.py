9cf40c10966428541f3262a4dec8a38fb6fe0d92
hongzhi <zanghz21@mails.tsinghua.edu.cn>
2025-12-16 22:01:59 +0800

    feat: update sac loss metrics

--- rlinf/workers/actor/fsdp_actor_worker_sac.py
+++ rlinf/workers/actor/fsdp_actor_worker_sac.py
@@ -422,72 +422,71 @@ class EmbodiedSACFSDPActor(EmbodiedFSDPActor):
             )
 
             self.qf_optimizer.zero_grad()
-            gbs_critic_loss = 0
+            gbs_critic_loss = []
             for batch in train_micro_batch_list:
                 batch = put_tensor_device(batch, device=self.device)
                 critic_loss = self.forward_critic(batch) / self.gradient_accumulation
                 critic_loss.backward()
-                gbs_critic_loss += critic_loss.item()
+                gbs_critic_loss.append(critic_loss.item())
             qf_grad_norm = self.model.clip_grad_norm_(
                 max_norm=self.cfg.actor.optim.clip_grad
             )
             self.qf_optimizer.step()
 
+            metrics_data = {
+                "critic/lr": self.qf_optimizer.param_groups[0]["lr"], 
+                "sac/critic_loss": np.mean(gbs_critic_loss), 
+                "critic/grad_norm": qf_grad_norm, 
+            }
+
             if update_idx % self.critic_actor_ratio == 0 and train_actor:
                 self.optimizer.zero_grad()
-                gbs_actor_loss = 0
+                gbs_actor_loss = []
                 gbs_entropy = 0
                 for batch in train_micro_batch_list:
                     batch = put_tensor_device(batch, device=self.device)
                     actor_loss, entropy = self.forward_actor(batch) 
                     actor_loss = actor_loss / self.gradient_accumulation
                     actor_loss.backward()
-                    gbs_actor_loss += actor_loss.item()
+                    gbs_actor_loss.append(actor_loss.item())
                     gbs_entropy += entropy.item() / self.gradient_accumulation
                 actor_grad_norm = self.model.clip_grad_norm_(
                     max_norm=self.cfg.actor.optim.clip_grad
                 )
                 self.optimizer.step()
                     
-                    # Update temperature parameter if using automatic entropy tuning
+                # Update temperature parameter if using automatic entropy tuning
                 if hasattr(self, 'base_alpha') and self.base_alpha is not None:
                     self.alpha_optimizer.zero_grad()
-                    gbs_actor_loss = 0
+                    gbs_alpha_loss = []
                     for batch in train_micro_batch_list:
                         batch = put_tensor_device(batch, device=self.device)
                         alpha_loss = self.forward_alpha(batch) / self.gradient_accumulation
                         alpha_loss.backward()
-                        gbs_actor_loss += alpha_loss.item()
+                        gbs_alpha_loss.append(alpha_loss.item())
                     torch.distributed.all_reduce(self.base_alpha.grad, op=torch.distributed.ReduceOp.AVG)
                     alpha_grad_norm = torch.nn.utils.clip_grad_norm_(self.base_alpha, self.cfg.actor.optim.clip_grad)
                     self.alpha_optimizer.step()
-                    
-                
-                loss = gbs_actor_loss + gbs_critic_loss
+
                 self.lr_scheduler.step()
                 self.qf_lr_scheduler.step()
                 if hasattr(self, 'base_alpha') and self.base_alpha is not None:
                     self.alpha_lr_scheduler.step()
                 # Collect metrics
-                metrics_data = {
-                    "sac/total_loss": loss,
+                metrics.update({
                     "sac/alpha": self.alpha, 
-                    # "actor/grad_norm": grad_norm.detach().item(),
+                    "sac/actor_loss": np.mean(gbs_actor_loss),
                     "actor/lr": self.optimizer.param_groups[0]["lr"],
-                    "critic/lr": self.qf_optimizer.param_groups[0]["lr"], 
-                    "sac/actor_loss": gbs_actor_loss, 
-                    "sac/critic_loss": gbs_critic_loss, 
+                    "sac/alpha_loss": np.mean(gbs_alpha_loss), 
                     "actor/grad_norm": actor_grad_norm, 
-                    "critic/grad_norm": qf_grad_norm, 
                     "alpha/grad_norm": alpha_grad_norm, 
                     "actor/entropy": entropy, 
-                    # "sac/qf_values": all_data_q_values.mean().detach().item(), 
-                    # "sac/current_q": min_qf_pi.mean().detach().item(), 
                     "replay_buffer/size": len(self.replay_buffer),
                     "replay_buffer/utilization": len(self.replay_buffer) / self.replay_buffer.capacity
-                }
+                })
                 
-                append_to_dict(metrics, metrics_data)
+                
+            append_to_dict(metrics, metrics_data)
 
             # # Soft update target network
             if self.target_model_initialized and self.update_step % self.cfg.algorithm.get("target_update_freq", 1) == 0:
