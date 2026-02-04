32a878e48a95c1b510f3c3797af09aea22db07e0
yimingzhou2002 <yiming_zhou2002@163.com>
2026-02-04 07:49:59 +0000

    add searchr1-qdrant-support

---
+++ examples/searchr1/qdrant_scripts/build_qdrant_coll.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -ex
+
+# Build step 1: set your wiki_dataset corpus path
+# WIKI2018_DIR=/mnt/public/zhuchunyang_rl/hf_datasets/ASearcher-Local-Knowledge
+WIKI2018_DIR=/your/wiki_dataset/path
+corpus_file=$WIKI2018_DIR/wiki_corpus.jsonl
+
+# Build step 2: set your retriever model path
+retriever_name=e5
+# retriever_path=/mnt/public/zhuchunyang_rl/hf_models/e5-base-v2
+retriever_path=/your/retriever/model/path
+
+# Qdrant configuration
+# Build step 3: Install qdrant and set qdrant_path to qdrant dir
+# qdrant_path=/mnt/public/zhouyiming/qdrant
+qdrant_path=/your/qdrant/path
+qdrant_url=http://localhost:6333
+qdrant_collection_name=wiki_collection_m24_cef512
+hnsw_config='{"m":24,"ef_construct":512}'
+
+
+"$qdrant_path/qdrant" &
+qdrant_pid=$!
+sleep 5
+
+python3  ./build_qdrant_coll.py --corpus_path $corpus_file \
+                                            --retriever_name $retriever_name \
+                                            --retriever_model $retriever_path \
+                                            --qdrant_collection_name $qdrant_collection_name \
+                                            --qdrant_url $qdrant_url\
+                                            --hnsw_config $hnsw_config\
+                                            --build_parallel 16\
+
+echo "wait for qdrant stop"
+kill $qdrant_pid
+wait $qdrant_pid 2>/dev/null
+echo "qdrant is stopped"
---
+++ examples/searchr1/qdrant_scripts/launch_local_server_qdrant.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -ex
+
+# Launch step 1: set your wiki_dataset pages path
+# WIKI2018_DIR=/mnt/public/zhuchunyang_rl/hf_datasets/ASearcher-Local-Knowledge
+WIKI2018_DIR=/your/wiki_dataset/path
+pages_file=$WIKI2018_DIR/wiki_webpages.jsonl
+
+# Launch step 2: set your retriever model path
+retriever_name=e5
+retriever_path=/your/retriever/model/path
+# retriever_path=/mnt/public/zhuchunyang_rl/hf_models/e5-base-v2
+
+# Qdrant configuration
+# qdrant_path=/mnt/public/zhouyiming/qdrant
+qdrant_url=http://localhost:6333
+qdrant_collection_name=wiki_collection_m24_cef512
+qdrant_search_param='{"hnsw_ef":256}'
+
+python3 -u ./local_retrieval_server_qdrant.py \
+                                            --pages_path $pages_file \
+                                            --topk 3 \
+                                            --retriever_name $retriever_name \
+                                            --retriever_model $retriever_path \
+                                            --qdrant_collection_name $qdrant_collection_name \
+                                            --qdrant_url $qdrant_url\
+                                            --qdrant_search_param $qdrant_search_param\
+                                            --port 8000 \
+
+
