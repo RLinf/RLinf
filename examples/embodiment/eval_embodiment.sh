0ef91d8fd10ec285a259b1bb1d1e5f92ca43804e
Iron-Wph <27790564355@qq.com>
2026-02-07 10:54:54 +0800

    upload openpi setting

--- examples/embodiment/eval_embodiment.sh
+++ examples/embodiment/eval_embodiment.sh
@@ -6,8 +6,6 @@ export SRC_FILE="${EMBODIED_PATH}/eval_embodied_agent.py"
 
 export MUJOCO_GL="osmesa"
 export PYOPENGL_PLATFORM="osmesa"
-# export MUJOCO_GL="egl"
-# export PYOPENGL_PLATFORM="egl"
 export PYTHONPATH=${REPO_PATH}:$PYTHONPATH
 
 # Base path to the BEHAVIOR dataset, which is the BEHAVIOR-1k repo's dataset folder
@@ -22,26 +20,24 @@ export ISAAC_PATH=${ISAAC_PATH:-/path/to/isaac-sim}
 export EXP_PATH=${EXP_PATH:-$ISAAC_PATH/apps}
 export CARB_APP_PATH=${CARB_APP_PATH:-$ISAAC_PATH/kit}
 
-export ROBOTWIN_PATH="/mnt/public/peihong/codes/RoboTwin_RLinf_support"
+export ROBOTWIN_PATH="/path/to/RoboTwin"
 export PYTHONPATH=${REPO_PATH}:${ROBOTWIN_PATH}:$PYTHONPATH
 
 export HYDRA_FULL_ERROR=1
 
 if [ -z "$1" ]; then
-    # CONFIG_NAME="maniskill_ppo_openvlaoft"
-    # CONFIG_NAME="robotwin_place_empty_cup_grpo_pi_eval"
-    CONFIG_NAME="robotwin_adjust_bottle_clean_grpo_pi_eval"
+    CONFIG_NAME="maniskill_ppo_openvlaoft"
 else
     CONFIG_NAME=$1
 fi
 
 # NOTE: Set the active robot platform (required for correct action dimension and normalization), supported platforms are LIBERO, ALOHA, BRIDGE, default is LIBERO
-ROBOT_PLATFORM=${2:-${ROBOT_PLATFORM:-"ALOHA"}}
+ROBOT_PLATFORM=${2:-${ROBOT_PLATFORM:-"LIBERO"}}
 
 export ROBOT_PLATFORM
 echo "Using ROBOT_PLATFORM=$ROBOT_PLATFORM"
 
-LOG_DIR="${REPO_PATH}/logs/debug_openpi" #/$(date +'%Y%m%d-%H:%M:%S')"
+LOG_DIR="${REPO_PATH}/logs/$(date +'%Y%m%d-%H:%M:%S')" #/$(date +'%Y%m%d-%H:%M:%S')"
 MEGA_LOG_FILE="${LOG_DIR}/eval_embodiment.log"
 mkdir -p "${LOG_DIR}"
 CMD="python ${SRC_FILE} --config-path ${EMBODIED_PATH}/config/ --config-name ${CONFIG_NAME} runner.logger.log_path=${LOG_DIR}"
