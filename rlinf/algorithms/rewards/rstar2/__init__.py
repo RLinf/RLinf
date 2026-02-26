acf719fcb9f88a893ef8e498b8e3a95bc2012fb8
3098340041@qq.com <wangxiangyuan@infini-ai.com>
2026-02-26 11:53:38 +0800

    update rstar2
    Signed-off-by: nufukim <3098340041@qq.com>

--- rlinf/algorithms/rewards/rstar2/__init__.py
+++ rlinf/algorithms/rewards/rstar2/__init__.py
@@ -46,7 +46,9 @@ def _compute_score_wrapper(
 
 class Rstar2Reward:
     def __init__(self, config: DictConfig):
-        assert HAS_COMPUTE_SCORE, "compute_score is not installed, please install it by running `pip install pylatexenc & pip install sympy`"
+        assert HAS_COMPUTE_SCORE, (
+            "compute_score is not installed, please install it by running `pip install pylatexenc & pip install sympy`"
+        )
         self.scale = config.get("reward_scale", 1.0)
         self.timeout = config.get("compute_score_timeout", 6.0)
         self.default_score = config.get("default_score_on_timeout", 0.0)
