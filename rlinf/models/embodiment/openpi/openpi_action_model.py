fdfceffe85cb064b969295b9f3a36fc57204347b
xuxin <694519875@qq.com>
2026-04-16 20:11:02 +0800

    feat: realworld pnp pi0 sft deploy
    Signed-off-by: xuxin <694519875@qq.com>

--- rlinf/models/embodiment/openpi/openpi_action_model.py
+++ rlinf/models/embodiment/openpi/openpi_action_model.py
@@ -492,7 +492,12 @@ class OpenPi0ForRLActionPrediction(PI0Pytorch, BasePolicy):
             processed_obs["observation/wrist_image"] = env_obs["wrist_images"]
         # extra view image observation
         if env_obs["extra_view_images"] is not None:
-            processed_obs["observation/extra_view_image"] = env_obs["extra_view_images"]
+            extra = env_obs.get("extra_view_images")
+            # extra_view_images: [N_ENV, N_IMG, H, W, C] -> take first view [N, H, W, C]
+            if extra.dim() == 5:
+                processed_obs["observation/extra_view_image"] = extra[:, 0]
+            else:
+                processed_obs["observation/extra_view_image"] = extra
         # store used keys
         return processed_obs
 
