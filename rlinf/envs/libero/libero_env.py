8f21eca5e917eaae5024bfc0e51c491ee8bc40d4
Iron-Wph <2779056435@qq.com>
2026-08-21 13:31:27 +0000

    fix(libero): default reset wait steps to zero
    Signed-off-by: Iron-Wph <2779056435@qq.com>

--- rlinf/envs/libero/libero_env.py
+++ rlinf/envs/libero/libero_env.py
@@ -840,7 +840,7 @@ class LiberoEnv(gym.Env):
 
         self._reconfigure(reset_state_ids, env_idx)
         # FastWAM's official evaluator waits for 30 open-gripper no-op steps.
-        num_steps_wait = int(self.cfg.get("num_steps_wait", 15))
+        num_steps_wait = int(self.cfg.get("num_steps_wait", 0))
         for _ in range(num_steps_wait):
             zero_actions = np.zeros((len(env_idx), 7))
             if self.cfg.reset_gripper_open:
