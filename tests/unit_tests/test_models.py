b1b61cfccb41cb12471d4cc435648d2fd0da7063
SCreatorX <2939063900@qq.com>
2026-09-21 23:32:09 +0800

    style(openwam): format merged model tests
    Signed-off-by: SCreatorX <2939063900@qq.com>

--- tests/unit_tests/test_models.py
+++ tests/unit_tests/test_models.py
@@ -1530,6 +1530,8 @@ def test_apxinf_a_missing_apxinf_robo_names_what_to_install(monkeypatch):
         OpenPIApxInfAdapter(
             _apxinf_model_cfg(), "cpu", processor=_FakeApxInfProcessor()
         )
+
+
 # --- OpenWAM checkpoint export ---------------------------------------------
 
 
